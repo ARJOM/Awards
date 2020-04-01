196c9b5ac93a43320dbcbe845cfefa106b9cd344
Ricart <ricarteusuo11@gmail.com>
2020-04-01 03:56:22 -0300

    feat(frontend): add login feature

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,15 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import './styles.css'
+import {useForm} from "react-hook-form";
+import api from "../../services/api";
 
-export default function Register() {
+export default function Login() {
+
+    const { register, handleSubmit } = useForm();
+
+    const history = useHistory();
+
+    const onSubmit = async data => {
+        try {
+            console.log(data);
+            const response = await api.post('session', data);
+            if (response.data.result){
+
+                history.push('/home');
+            }
+        } catch (e) {
+            alert("Nome de usuário ou senha incorretos")
+        }
+    };
     return(
         <div className="logon-container">
             <section className="form">
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <h1>Faça seu login</h1>
-                    <input placeholder="Username"/>
-                    <input type="password" placeholder="Senha"/>
+                    <input
+                        placeholder="Nome de usuário"
+                        name="username"
+                        ref={register({required: true})}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Senha"
+                        name="password"
+                        ref={register({required: true})}
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">

41d2d609c70821df382bf32d4760bb9c430f95b4
Ricart <ricarteusuo11@gmail.com>
2020-04-05 14:53:46 -0300

    feat(frontend): add photos list by category

--- frontend/src/pages/Category/index.js
+++ frontend/src/pages/Category/index.js
@@ -1,39 +1,47 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import smile from "../../assets/sorriso.jpeg";
+import React, {useEffect, useState} from "react";
+import {Link, useParams} from "react-router-dom";
 import './styles.css';
 import Rate from "../Rate";
 import Header from "../Header";
+import api from "../../services/api";
 
 export default function Category() {
+    const [photos, setPhotos] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [category, setCategory] = useState("");
+
+    const { id } = useParams();
+
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        api.get('photos/'+id, {
+            headers: {
+                Authorization: token
+            }
+        }).then(response => {
+            setPhotos(response.data['photos']);
+            setTotal(response.data['total-count']);
+            setCategory(response.data['category']);
+        })
+    }, [token]);
+
     return(
         <div className="category-container">
             <Header/>
 
-            <h1>Fotos de Categoria</h1>
+            <h1>Fotos de {category} - {total}</h1>
 
             <ul>
-                <li>
-                    <Link to="/detail">
-                        <strong>Username</strong>
-                    </Link>
-                    <img src={smile} width={900} alt="category name"/>
-                    <Rate/>
-                </li>
-                <li>
-                    <Link to="/detail">
-                        <strong>Username</strong>
-                    </Link>
-                    <img src={smile} width={900} alt="category name"/>
-                    <Rate/>
-                </li>
-                <li>
-                    <Link to="/detail">
-                        <strong>Username</strong>
-                    </Link>
-                    <img src={smile} width={900} alt="category name"/>
-                    <Rate/>
-                </li>
+                {photos.map(photo => (
+                    <li>
+                        <Link to={"/detail/"+photo.id}>
+                            <strong>{photo.username}</strong>
+                        </Link>
+                        <img src={photo.photo} width={900} alt={"Foto de "+photo.username}/>
+                        <Rate/>
+                    </li>
+                ))}
             </ul>
 
         </div>
